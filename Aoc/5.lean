69257df9fc4cc13e79ede775f6aacc8fd7524b99
Sebastian Ullrich <sebasti@nullri.ch>
2022-12-05 21:34:22 +0100

    a bit nicer

--- Aoc/5.lean
+++ Aoc/5.lean
@@ -5,13 +5,8 @@ aoc (input : Pair (Lines String) "\n\n" (Lines Words)) => Id.run do
   let numsLine := input.1[input.1.size-1]!
   let numCols := numsLine.splitOn |>.filter (!·.isEmpty) |>.length
   let mut stacks :=
-    List.range numCols |>.map (fun col =>
-      crates.filterMap (fun line =>
-        match line.get? ⟨1 + 4*col⟩ with
-        | some ' ' => none
-        | c? => c?)
-      |>.toList)
-    |>.toArray
+    List.range numCols |>.toArray.map (fun col =>
+      crates.filterMap (·.get? ⟨1 + 4*col⟩ |>.filter (· != ' ')) |>.toList)
 
   for cmd in input.2 do
     let #[_, n, _, from_, _, to] := cmd | unreachable!
@@ -19,26 +14,21 @@ aoc (input : Pair (Lines String) "\n\n" (Lines Words)) => Id.run do
     let to := to.toNat! - 1
     for _ in [0:n.toNat!] do
       let .cons c rest := stacks[from_]! | unreachable!
-      stacks := stacks.set! from_ rest |>.set! to (c :: stacks[to]!)
-  List.range numCols |>.map (fun col => stacks[col]!.head!) |> String.mk
+      stacks := stacks.set! from_ rest |>.modify to (c :: ·)
+  List.range numCols |>.map (stacks[·]!.head!) |> String.mk
 
 aoc (input : Pair (Lines String) "\n\n" (Lines Words)) => Id.run do
   let crates := input.1.pop
   let numsLine := input.1[input.1.size-1]!
   let numCols := numsLine.splitOn |>.filter (!·.isEmpty) |>.length
   let mut stacks :=
-    List.range numCols |>.map (fun col =>
-      crates.filterMap (fun line =>
-        match line.get? ⟨1 + 4*col⟩ with
-        | some ' ' => none
-        | c? => c?)
-      |>.toList)
-    |>.toArray
+    List.range numCols |>.toArray.map (fun col =>
+      crates.filterMap (·.get? ⟨1 + 4*col⟩ |>.filter (· != ' ')) |>.toList)
 
   for cmd in input.2 do
     let #[_, n, _, from_, _, to] := cmd | unreachable!
     let from_ := from_.toNat! - 1
     let to := to.toNat! - 1
     let n := n.toNat!
-    stacks := stacks.set! from_ (stacks[from_]!.drop n) |>.set! to (stacks[from_]!.take n ++ stacks[to]!)
-  List.range numCols |>.map (fun col => stacks[col]!.head!) |> String.mk
+    stacks := stacks.modify from_ (·.drop n) |>.modify to (stacks[from_]!.take n ++ ·)
+  List.range numCols |>.map (stacks[·]!.head!) |> String.mk
